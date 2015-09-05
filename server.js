94e7a5258353da5733bb458de2055f4cbc8d1e35
Ivan Maga <magaivan1987@gmail.com>
2015-09-05 09:25:38 +0300

    дороблено сервер

--- server.js
+++ server.js
@@ -3,12 +3,16 @@
  */
 var http = require("http");
 
-http.createServer(function(req, res) {
-    console.log("request ");
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.write("Hello World");
-    res.end();
-}).listen(8888);
+function start() {
+    function onRequest(request, response) {
+        console.log("request ");
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("Hello World");
+        res.end();
+    }
 
-console.log("server isload")
+    http.createServer(onRequest).listen(8888);
 
+    console.log("Server has started.")
+}
+start()
